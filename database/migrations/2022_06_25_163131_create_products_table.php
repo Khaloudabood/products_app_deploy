49c7970c0fefcf5fbf287e95455593be091653f2
unknown <k.khusaibi@gmail.com>
2022-06-25 21:33:54 +0400

    first commit

---
+++ database/migrations/2022_06_25_163131_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('product_name');
+            $table->string('product_type');
+            $table->string('product_color');
+            $table->string('img');
+            $table->integer('price');
+            $table->integer('size');
+            $table->string('product_section');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
